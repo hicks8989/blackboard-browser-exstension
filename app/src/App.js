5e55aecf97b19d83896d43a7886ac07ce41549a4
Eric Hicks <eric.hicks.8989@gmail.com>
2020-07-13 12:54:57 -0400

    feat: Created script to generate dist folder
    - Gitignored `dist` folder.
    - Updated `manifest.json`.

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+// Import React:
+import React, { Component } from 'react';
+
+// Main app class:
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-xl-center">Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
